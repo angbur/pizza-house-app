527c18dd8e5ffafa113622a0d3bccf8d1a60cf72
angbur <ag.grala@gmail.com>
2022-12-22 10:56:48 +0100

    feat: add basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
+import { Routes, Route, Outlet } from 'react-router-dom';
 import { Fragment, useContext } from 'react';
 import Header from './components/Organisms/Header/Header';
 import { ThemeContext } from './components/Theme/ThemeContext';
 import GlobalStyle from './global';
+import Main from './components/Page/Main/Main';
 
 const App = () => {
   const theme = useContext(ThemeContext);
   return (
     <Fragment>
-      <Header theme={theme} />
       <GlobalStyle theme={theme} />
+      <Header theme={theme} />
+      <Outlet/>
+      <Routes>
+        <Route path='/' element={<Main/>} />
+        <Route path="*" element={<Main />} />
+      </Routes>
     </Fragment>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import ThemeProvider from './components/Theme/ThemeContext';
 
@@ -7,7 +8,9 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>,
 );
